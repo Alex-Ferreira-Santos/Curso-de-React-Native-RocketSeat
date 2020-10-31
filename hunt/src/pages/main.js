ac640fe787cdb4cb2a1121de557a111ab23dbb7b
Alex-Ferreira-Santos <alexfstos@gmail.com>
2020-10-31 14:03:52 -0300

    entendendo o estado

--- hunt/src/pages/main.js
+++ hunt/src/pages/main.js
@@ -7,6 +7,11 @@ export default class Main extends Component{
         title: "JSHunt"
     }
 
+    state = {
+        docs: [],
+    }
+
+
     componentDidMount(){
         this.loadProduct()
     }
@@ -15,13 +20,14 @@ export default class Main extends Component{
         const response = await api.get('/products')
         const { docs } = response.data
 
-        console.log(docs)
+        this.setState({docs})
     }
 
     render(){
         return(
             <View>
-                <Text>Página Main</Text>
+                <Text>Página Main:</Text>
+                {this.state.docs.map(product => <Text key={product._id}>{product.title}</Text>)}
             </View>
         )
     }
